672e532dd87520c76fda6b7483de68b2f0fcf99a
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-12-07 23:54:07 -0700

    Set up server file with Express, basic HTML routes

---
+++ server.js
@@ -0,0 +1,29 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+
+//Set up Express
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+//=================================================
+
+//HTML route for home page
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+//HTML route to notes page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+//Start server
+app.listen(PORT, function () {
+    console.log("Server listening on port: " + PORT)
+});
